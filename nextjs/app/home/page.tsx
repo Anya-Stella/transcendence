e58d7a42514aada51adbeac3257e0232add33b3f
reprise39 <reprise39@yahoo.co.jp>
2026-03-19 20:37:51 +0900

    remove jose(apply all authjs)

--- nextjs/app/home/page.tsx
+++ nextjs/app/home/page.tsx
@@ -1,29 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
-
-interface User {
-	id: string;
-	name: string;
-	email: string;
-}
+import { useSession, signOut } from "next-auth/react";
 
 export default function HomePage() {
 	const router = useRouter();
-	const [user, setUser] = useState<User | null>(null);
-
-	useEffect(() => {
-		fetch("/api/me")
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.user) 	setUser(data.user);
-				
-			})
-			.catch(() => { });
-	}, []);
+	const { data: session } = useSession();
+	const user = session?.user;
 
 	const handleLogout = async () => {
 		await signOut({ callbackUrl: "/login" });
